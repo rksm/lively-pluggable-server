6a178a550e0f9158814f9e34ae2c179e21b00e97
Robert Krahn <robert.krahn@gmail.com>
2013-08-01 12:02:02 -0700

    support for mutliple servers

--- index.js
+++ index.js
@@ -1,52 +1,55 @@
 var debug = false,
     defaultPort = 9003,
-    defaultRoute = '',
+    defaultRoute = '/',
     request = require('request'),
     restify = require('restify'),
-    testServer, serverURL;
+    servers = [];
 
-function serverSetup(server) {
-    testServer.on('close', onClose);
-    server.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS, PROPFIND, REPORT');
-        res.header("Access-Control-Allow-Headers", "X-Requested-With, Depth, Content-Type");
-        res.header("Access-Control-Expose-Headers", "Date, Etag");
-        next();
-    });
+function cors(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS, PROPFIND, REPORT');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Depth, Content-Type");
+    res.header("Access-Control-Expose-Headers", "Date, Etag");
+    next();
 }
 
-function onClose() {
+function onClose(server) {
     debug && console.log('test server closed');
+    for (var i = 0; i < servers.length; i++) {
+        if (servers[i].server === server) {
+            servers.splice(i, 1); return; }
+    }
 }
 
-function startTestServer(options, thenDo) {
+function startServer(options, thenDo) {
     options = options || {};
-    testServer = restify.createServer({name: options.name})
-    serverSetup(testServer);
-    testServer.listen(options.port || defaultPort, function() {
-        serverURL = testServer.url;
+    var server = restify.createServer({name: options.name});
+    servers.push({server: server});
+    server.on('close', onClose.bind(null, server));
+    server.use(cors);
+    server.listen(options.port || defaultPort, function() {
         if (options.subservers) {
             options.subservers.forEach(function(subserverRegistrationFunc) {
-                subserverRegistrationFunc(options.route || defaultRoute, testServer);
+                subserverRegistrationFunc(subserverRegistrationFunc.route || defaultRoute, server);
             });
         }
-        debug && console.log('%s listening at %s', testServer.name, testServer.url);
-        thenDo && thenDo(null, testServer);
+        debug && console.log('%s listening at %s', server.name, server.url);
+        thenDo && thenDo(null, server);
     });
 }
 
-function stopTestServer(thenDo) {
-    testServer.close(thenDo);
+function stopServer(server, thenDo) {
+    server.close(thenDo);
 }
 
-function doRequest(method, path, options, callback) {
-    request[method.toLowerCase()](serverURL + path, options, callback);
+function doRequest(method, server, path, options, callback) {
+    request[method.toLowerCase()](server.url + path, options, callback);
 }
 
 module.exports = {
-    start: startTestServer,
-    stop: stopTestServer,
+    start: startServer,
+    stop: stopServer,
     request: doRequest,
-    post: doRequest.bind(null, 'post')
+    post: doRequest.bind(null, 'post'),
+    servers: servers
 };
