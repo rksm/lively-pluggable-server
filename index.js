846b8fe6c738ec2b8685f8a3ddcd3136f6d8aa0b
Robert Krahn <robert.krahn@gmail.com>
2013-08-02 17:38:20 -0700

    reload config

--- index.js
+++ index.js
@@ -10,37 +10,23 @@ var debug = false,
     restify = require('restify'),
     servers = [];
 
-function cors(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS, PROPFIND, REPORT');
-    res.header("Access-Control-Allow-Headers", "X-Requested-With, Depth, Content-Type");
-    res.header("Access-Control-Expose-Headers", "Date, Etag");
-    next();
-}
-
-function onClose(server) {
-    debug && console.log('test server closed');
-    for (var i = 0; i < servers.length; i++) {
-        if (servers[i].server === server) {
-            servers.splice(i, 1); return; }
-    }
-}
-
-function registerSubserver(server, subserverRegFunc) {
-    var oldRoutes = util._extend({}, server.routes);
-    if (!subserverRegFunc.routes) subserverRegFunc.routes = [];
-    subserverRegFunc(subserverRegFunc.route || defaultRoute, server);
-    Object.keys(server.routes).forEach(function(routeName) {
-        if (!(routeName in oldRoutes)) subserverRegFunc.routes.push(routeName); });
-}
-
-function unregisterSubserver(server, subserver) {
-    if (!subserver.routes) return;
-    subserver.routes.forEach(server.rm.bind(server));   
-    subserver.routes.length = 0;
-}
-
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// server start/stop
 function startServer(options, thenDo) {
+    function cors(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS, PROPFIND, REPORT');
+        res.header("Access-Control-Allow-Headers", "X-Requested-With, Depth, Content-Type");
+        res.header("Access-Control-Expose-Headers", "Date, Etag");
+        next();
+    }
+    function onClose(server) {
+        debug && console.log('test server closed');
+        for (var i = 0; i < servers.length; i++) {
+            if (servers[i].server === server) {
+                servers.splice(i, 1); return; }
+        }
+    }
     options = options || {};
     var server = restify.createServer({name: options.name});
     servers.push({server: server});
@@ -57,7 +43,29 @@ function stopServer(server, thenDo) {
     server.close(thenDo);
 }
 
-function loadFromConfig(server, configFileName, thenDo) {
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// subservers
+function registerSubserver(server, subserverRegFunc) {
+    var oldRoutes = util._extend({}, server.routes);
+    if (!subserverRegFunc.routes) subserverRegFunc.routes = [];
+    subserverRegFunc(subserverRegFunc.route || defaultRoute, server);
+    Object.keys(server.routes).forEach(function(routeName) {
+        if (!(routeName in oldRoutes)) subserverRegFunc.routes.push(routeName); });
+}
+
+function unregisterSubserver(server, subserver) {
+    if (!subserver.routes) return;
+    subserver.routes.forEach(server.rm.bind(server));
+    subserver.routes.length = 0;
+}
+
+function unregisterRoutes(server) {
+    server.routes && Object.keys(server.routes).forEach(server.rm.bind(server));
+}
+
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// config
+function loadConfig(server, configFileName, thenDo) {
     var fullFilename;
     async.waterfall([
         function(next) {
@@ -76,6 +84,11 @@ function loadFromConfig(server, configFileName, thenDo) {
     ], function(err) { err && console.error(err); thenDo(err); });
 }
 
+function reloadConfig(server, configFileName, thenDo) {
+    unregisterRoutes(server);
+    loadConfig(server, configFileName, thenDo);
+}
+
 function setupSubserversFromConfig(server, config, thenDo) {
     try {
         if (!config.subservers) thenDo(null);
@@ -89,21 +102,26 @@ function setupSubserversFromConfig(server, config, thenDo) {
     thenDo(null);
 }
 
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// server communication
 function doRequest(method, server, path, options, callback) {
     request[method.toLowerCase()](server.url + path, options, callback);
 }
 
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// export
 module.exports = {
     start: startServer,
     stop: stopServer,
     load: registerSubserver,
     unload: unregisterSubserver,
     // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-    loadFromConfig: loadFromConfig,
+    loadConfig: loadConfig,
+    reloadConfig: reloadConfig,
     // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
     request: doRequest,
     post: doRequest.bind(null, 'post'),
     get: doRequest.bind(null, 'get'),
     // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
     servers: servers
-};
+}
--- tests.js
+++ tests.js
@@ -38,25 +38,43 @@ var tests = {
         test.deepEqual(subserver.routes, [], 'routes');
         test.done();
     },
-    testSubserverFromFile: function (test) {
+    testSubserverConfig: function (test) {
         var subserverSource = "module.exports = function(baseRoute, app) {\n"
                             + "    app.get(baseRoute, function(req, res) {\n"
                             + "        res.end('hello'); });\n"
                             + "}",
-            configSource = '{"subservers":{"subserver.js":{"route":"/test2"}}}'
-        fsHelper.createDirStructure('.', {
-            testSubserverFromFile: {
-                "subserver.js": subserverSource,
-                "config.json": configSource
+            configSource = '{"subservers":{"subserver.js":{"route":"/test2"}}}',
+            files = {testSubserverFromFile: {"subserver.js": subserverSource, "config.json": configSource}};
+        async.waterfall([
+            fsHelper.createDirStructure.bind(null, '.', files),
+            serverManager.loadConfig.bind(null, server, "testSubserverFromFile/config.json"),
+            serverManager.get.bind(null, server,'/test2'), // --> res, body
+            function assert(res, body, next) {
+                test.equal(body, 'hello', 'subserver get'); next();
             }
-        }, function() {
-            serverManager.loadFromConfig(server, "testSubserverFromFile/config.json", function(err) {
-                serverManager.get(server,'/test2', function(err, res, body) {
-                    test.equal(body, 'hello', 'subserver get');
-                    test.done();
-                });            
-            });
-        });
+        ], test.done);
+    },
+    testSubserverConfigReload: function (test) {
+        var subserverSource = "module.exports = function(baseRoute, app) {\n"
+                            + "    app.get(baseRoute, function(req, res) {\n"
+                            + "        res.end('hello'); });\n"
+                            + "}",
+            configSource1 = '{"subservers":{"subserver.js":{"route":"/test1"}}}',
+            configSource2 = '{"subservers":{"subserver.js":{"route":"/test2"}}}',
+            files = {testSubserverFromFile: {"subserver.js": subserverSource, "config1.json": configSource1, "config2.json": configSource2}};
+        async.waterfall([
+            fsHelper.createDirStructure.bind(null, '.', files),
+            serverManager.reloadConfig.bind(null, server, "testSubserverFromFile/config1.json"),
+            serverManager.reloadConfig.bind(null, server, "testSubserverFromFile/config2.json"),
+            serverManager.get.bind(null, server,'/test1'), // --> res, body
+            function(res, body, next) {
+                test.equal(404, res.statusCode, 'subserver get old'); next();
+            },
+            serverManager.get.bind(null, server,'/test2'), // --> res, body
+            function(res, body, next) {
+                test.equal(body, 'hello', 'subserver get'); next();
+            }
+        ], test.done);
     }
 };
 
