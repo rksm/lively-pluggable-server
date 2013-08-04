8a3f8ede6c75c0df34e6651608741fbf1cdbb43e
Robert Krahn <robert.krahn@gmail.com>
2013-08-03 17:46:41 -0700

    extending the CORS support

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ var debug = false,
 function startServer(options, thenDo) {
     function cors(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
+        res.header('Access-Control-Allow-Credentials', 'true');
         res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS, PROPFIND, REPORT');
-        res.header("Access-Control-Allow-Headers", "X-Requested-With, Depth, Content-Type");
-        res.header("Access-Control-Expose-Headers", "Date, Etag");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With, Depth, Cookie, Set-Cookie, Accept, Access-Control-Allow-Credentials, Origin, Content-Type, Request-Id , X-Api-Version, X-Request-Id, Authorization");
+        res.header("Access-Control-Expose-Headers", "Date, Etag, Set-Cookie");
         next();
     }
     function onClose(server) {
